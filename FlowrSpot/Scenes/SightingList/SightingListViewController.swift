6c9b5d310c8609b53dbbe0b5dd1e333f37a4b0e4
AlmirGaric <almirgaric@AlmirGarics-MacBook-Pro.local>
2023-10-08 17:46:08 +0200

    Added a tableView to Screen SightList

--- FlowrSpot/Scenes/SightingList/SightingListViewController.swift
+++ FlowrSpot/Scenes/SightingList/SightingListViewController.swift
@@ -7,37 +7,58 @@
 //
 
 import UIKit
+import SnapKit
 
 class SightingListViewController: UIViewController {
 
+    var tableView : UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.white
+        
+        
+        tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
 
-        // Create a UILabel
-        let label = UILabel()
+        //  tableView.dataSource = self
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        view.addSubview(tableView)
+        
+        // Use SnapKit to set up Auto Layout constraints
+                tableView.snp.makeConstraints { make in
+                    make.edges.equalToSuperview() // This will make the table view fill its superview
+                }
 
-        // Configure label properties
-        label.text = "Hello, World!"
-        label.font = UIFont.systemFont(ofSize: 18)
-        label.textColor = UIColor.black
-        label.textAlignment = .center
-
-        // Add the label as a subview
-        view.addSubview(label)
-
-        // Set the label's frame or layout constraints
-        label.translatesAutoresizingMaskIntoConstraints = false
-
-        // Using Auto Layout constraints
-        NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
-
-        // Alternatively, you can set the frame manually
-        // label.frame = CGRect(x: 50, y: 100, width: 200, height: 30)
+        
+       
     }
 
+}
+
+extension SightingListViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            // Return the number of rows you want in your table
+            return 10
+        }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+          
+          // Configure the cell with data
+          cell.textLabel?.text = "Row \(indexPath.row)"
+         
+          return cell
+      }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
 }
 
