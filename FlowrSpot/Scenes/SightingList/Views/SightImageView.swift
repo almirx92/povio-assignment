69b43910bc553944d63adbef0cd51fbec519fcf9
AlmirGaric <almirgaric@AlmirGarics-MacBook-Pro.local>
2023-10-09 21:07:48 +0200

    Made func setupView(), addSubViews() and layout()

--- FlowrSpot/Scenes/SightingList/Views/SightImageView.swift
+++ FlowrSpot/Scenes/SightingList/Views/SightImageView.swift
@@ -19,24 +19,28 @@ class SightImageView: UIView {
         super.init(frame: frame)
         // Initialize and configure your view here
         setupView()
+        addSubViews()
+        layout()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        // Initialize and configure your view here if it's being created from a storyboard or xib
-        setupView()
     }
     
     private func setupView() {
         // Configure your view here
-        backgroundImage
-        backgroundColor = UIColor.blue
-    }
-    private func test() {
         guard let url = URL(string: "https://images.unsplash.com/photo-1604085572504-a392ddf0d86a") else { return }
         sightImage.setImage(with: url)
         titleLabel.text = "Cviješće xd"
         titleLabel.backgroundColor = UIColor.white
     }
-    
+    private func addSubViews(){
+        addSubview(sightImage)
+        
+    }
+    private func layout(){
+        sightImage.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
 }
